8ccbbfd4f7fce8afb31f2df7493d031cb9043cc3
Russell Boyatt <Russell.Boyatt@warwick.ac.uk>
2019-08-16 14:06:23 +0100

    MOO-1497: version bump to force language string import.

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '2019081600';
+$plugin->version = '2019081601';
 $plugin->requires = '2018112800';
 $plugin->component = 'theme_boostwarwick';
 
